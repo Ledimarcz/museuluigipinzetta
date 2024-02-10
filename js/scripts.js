14262be10091f9ca57095065be7d67e72aabb499
Ledimarcz <ledi1907@gmail.com>
2024-02-10 13:42:03 -0300

    Feito a barra de navegação mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 $( document ).ready(function() {
 
-	
+	/* esconde a barra de navegação ao carregar a página caso a resolução da mesma seja maior que 768px*/
+	if ($(document).width()<=768) {
+		$( "#header-desktop" ).hide();
+	} else {
+        $( "#header-mobile" ).hide();
+    }
+
+    /* mostra e esconde a barra de navegação baseado na mudança de resolução da tela */
+	$(window).on('resize', function(){
+    	var win = $(this);
+    	if (win.width() <=768) {
+    		$( "#header-desktop" ).hide();
+    		$( "#header-mobile" ).show();
+    	} else {
+    		$( "#header-desktop" ).show();
+    		$( "#header-mobile" ).hide();
+    	}
+	});
+
+    /* esconde a barra de navegação ao carregar a página */
+    $( "#navbar-mobile" ).hide();
+
+    /* mostra e esconde a barra de navegação */
+	$( "#btn-menu" ).on( "click", function() {
+        $( "#navbar-mobile" ).slideToggle( "slow" );
+        $(this).toggleClass('fa-bars fa-xmark');
+    });
     
 });
